eadb11f15c896b42e6eb26bce148557ed47e4f36
Yewon Jeong <yewon7036@gmail.com>
2022-03-17 21:58:50 +0900

    Initial commit - 롤링페이퍼 프로젝트

---
+++ src/main/java/uomah/rollingpaperproject/RollingpaperProjectApplication.java
@@ -0,0 +1,13 @@
+package uomah.rollingpaperproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingpaperProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingpaperProjectApplication.class, args);
+	}
+
+}
